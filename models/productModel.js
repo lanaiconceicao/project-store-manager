be7ab63250e1a142509bae03e0341ec28f51a438
Lanai Conceição <conceicao.lanai@gmail.com>
2022-01-25 14:48:58 -0300

    Changing the code from tests model to model... ops

---
+++ models/productModel.js
@@ -0,0 +1,17 @@
+const connection = require('./connection');
+
+const add = async (name, quantity) => {
+  const [result] = await connection
+    .query('INSERT INTO products (name, quantity) VALUES (?, ?);',
+      [name, quantity]);
+  
+  return {
+    id: result.insertId,
+    name,
+    quantity,
+  };
+};
+
+module.exports = {
+  add,
+};
