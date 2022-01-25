8ce701e05827345b7ae25b349cd27614c3258862
Lanai Conceição <conceicao.lanai@gmail.com>
2022-01-25 14:46:07 -0300

    index.js: Express and bodyParser imported

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
