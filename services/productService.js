354ad8e18f090ce5e63d265f55d56bc4d5193357
Lanai Conceição <conceicao.lanai@gmail.com>
2022-01-26 15:10:32 -0300

    Req 2: Service created

--- services/productService.js
+++ services/productService.js
@@ -2,6 +2,15 @@ const productModel = require('../models/productModel');
 
 const add = async ({ name, quantity }) => productModel.add(name, quantity);
 
+const getAll = async () => productModel.getAll();
+
+const getById = async (id) => {
+  const product = await productModel.getById(id);
+  return product;
+};
+
 module.exports = {
   add,
+  getAll,
+  getById,
 };
