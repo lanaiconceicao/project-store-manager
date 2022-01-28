aa8da010524dab11bbdf5c99db66d02f1e59d840
Lanai Conceição <conceicao.lanai@gmail.com>
2022-01-28 15:47:46 -0300

    Req 6: getAll & getById (model) created

--- models/salesModel.js
+++ models/salesModel.js
@@ -1,6 +1,6 @@
 const connection = require('./connection');
 
-// Função add() feita com a ajuda da pessoa estudante Gessé Carlos
+// Feito com a ajuda da pessoa estudante Gessé Carlos
 const add = async (sales) => {
   const [response] = await connection.query(
     'INSERT INTO sales (date) VALUES (NOW())',
@@ -20,6 +20,39 @@ const add = async (sales) => {
   return response;
 };
 
+const getAll = async () => {
+  const [response] = await connection.query(
+    `SELECT salesprod.sale_id AS saleId,
+    sales.date AS date,
+    salesprod.product_id AS product_id,
+    salesprod.quantity AS quantity
+    FROM sales_products AS salesprod
+    INNER JOIN sales AS sales
+    ON sales.id = salesprod.sale_id;
+    `,
+  );
+  return response;
+};
+
+const getById = async (id) => {
+  const [response] = await connection.query(
+    `SELECT sales.date AS date,
+    salesprod.product_id AS product_id,
+    salesprod.quantity AS quantity
+    FROM sales_products AS salesprod
+    INNER JOIN sales AS sales
+    ON sales.id = salesprod.sale_id
+    WHERE salesprod.sale_id = ?
+    `, [id],
+  );
+
+  if (!response.length) return null;
+
+  return response;
+};
+
 module.exports = {
   add,
+  getAll,
+  getById,
 };
