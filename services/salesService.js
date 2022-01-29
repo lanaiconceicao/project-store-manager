5cfbbfc54b7e32f85573b00f747866a96a8629af
Lanai Conceição <conceicao.lanai@gmail.com>
2022-01-29 08:46:44 -0300

    Req 10: Service created

--- services/salesService.js
+++ services/salesService.js
@@ -1,4 +1,5 @@
 const salesModel = require('../models/salesModel');
+const productService = require('./productService');
 
 const add = async (sales) => {
   const result = salesModel.add(sales);
@@ -21,9 +22,25 @@ const update = async ({ product_id: id, quantity }) => {
   return updatedSale;
 };
 
+// Feito com ajuda do estudante Gessé Carlos
+const remove = async (id) => {
+  if (!id) return null;
+  const [sale] = await getById(id);
+
+  const product = await productService.getById(sale.product_id);
+  product.quantity += sale.quantity;
+
+  const removeSale = await salesModel.remove(id);
+
+  await productService.update(product);
+
+  return removeSale;
+};
+
 module.exports = {
   add,
   getAll,
   getById,
   update,
+  remove,
 };
